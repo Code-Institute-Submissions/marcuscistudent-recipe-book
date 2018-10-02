54318460f4f8321ab9498b85332c922599d96ff1
Marcus Hornby <marcushornby@outlook.com>
2018-10-02 07:21:40 +0000

    connect to mLab

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipes'
+app.config["MONGO_URI"] = 'mongodb://marcus:rugby4me@ds221003.mlab.com:21003/recipes'
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello Recipe Book'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+    recipes=mongo.db.Recipes.find())
+
     
 
 if __name__ == '__main__':

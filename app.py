ee0b4f27d3bcf9e99ed6e43d21887ec23a409d9f
Marcus Hornby <marcushornby@outlook.com>
2019-02-01 19:11:31 +0000

    added search function

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, jsonify, flash
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import json
+
 
 
 
@@ -9,15 +11,16 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipes'
 app.config["MONGO_URI"] = 'mongodb://marcus:rugby4me@ds221003.mlab.com:21003/recipes'
 
+
+
 mongo = PyMongo(app)
 
 
 
-@app.route('/')
+
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", 
-    recipes=mongo.db.the_recipes.find())
+    return render_template("recipes.html", recipes=mongo.db.the_recipes.find())
     
     
 @app.route('/add_recipe')
@@ -43,18 +46,19 @@ def update_recipe(recipe_id):
     recipes = mongo.db.the_recipes
     recipes.update( {'_id': ObjectId(recipe_id)},
     {
-        'recipe_name':request.form.get['recipe_name'],
-        'category_name':request.form.get['category_name'],
-        'ingredients': request.form.get['ingredients'],
-        'method': request.form.get['method']
+        'recipe_name':request.form.get('recipe_name'),
+        'category_name':request.form.get('category_name'),
+        'ingredients': request.form.get('ingredients'),
+        'method': request.form.get('method')
     })
     return redirect(url_for('get_recipes'))
+    
     
     
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.the_recipes.remove({'_id': ObjectId(recipe_id)})
-    return redirect(url_for('get_tasks'))
+    return redirect(url_for('get_recipes'))
 
 @app.route('/get_categories')
 def get_categories():
@@ -90,9 +94,25 @@ def insert_category():
 @app.route('/new_category')
 def new_category():
     return render_template('addcategory.html')
-
-
     
+
+
+@app.route('/')        
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    search_term = []
+    if request.method == 'POST':
+        search_term = request.form['recipe_name']
+    return render_template('index.html', the_recipe=mongo.db.the_recipes.find_one({"recipe_name": search_term}))
+       
+@app.route('/category_search', methods=['GET', 'POST'])
+def category_search():
+    search_term = []
+    if request.method == 'POST':
+        search_term = request.form['category_name']
+    return render_template('category_search.html', the_recipe=mongo.db.the_recipes.find_one({"category_name": search_term}))
+ 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
